07970523286c324ddc94181d9a7a5ab1b2bac7b4
lauregonnord <Laure.Gonnord@ens-lyon.fr>
2017-09-11 17:49:52 +0200

    step1 code

--- lab_1/_code/step1/main.c
+++ lab_1/_code/step1/main.c
@@ -1,14 +1,53 @@
+// SEC LAB 1 - Main File for students, to be completed
+
 #include <avr/io.h>
 #include <util/delay.h>
 
-void setup(void) {}
+//Red Led on Digital 13 - 1Hz
 
+void init(void)
+{
+  // DDRB is the configuration register for digital 7 to 18
+  DDRB |= 0b00100000;// Digital 13 "outputmode"
+  // TODO : enable write for digital 1 to 7 (7seg)
+  // TODO : initialize global state values 
+
+}
+
+
+void change_led_state(){
+  PORTB ^= 0b00100000; // Change digital 13 on->off->on (xor is life!)
+}
+
+
+int get_reset_value(){ // on digital 10 ie bit 2
+  return 1; // TODO CHANGE HERE
+}
+
+
+void display_7seg(int value){
+  switch (value) {
+  case 0: //a,b,c,d,e,f should be on
+    PORTD = 0b01111110;
+    break; //TODO : complete for other values.
+  default:
+    PORTD = 0b00000000; 
+  }
+}
+
+void change_7seg_state (){
+  return;
+}
+  
 int main(void)
 {
-  setup();
-  while(1)
-  {
-    // Business code goes here
-    _delay_ms(1000);
-  }
+  init();
+  while(1) //infinite loop
+    {
+      // display_7seg(0); // uncomment to test the 7-seg when DDRD is configured
+      change_led_state();
+      _delay_ms(1000);     // 1Hz period
+    }
+  
+  return 0;
 }
