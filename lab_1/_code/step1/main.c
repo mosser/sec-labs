9c308ad8706414b3bccc2681554bbbd264f0d7b7
gduboc <guillaume.duboc@ens-lyon.fr>
2018-09-16 21:58:15 +0200

    Add completed code for lab_1/step1

--- lab_1/_code/step1/main.c
+++ lab_1/_code/step1/main.c
@@ -30,24 +30,62 @@ void display_7seg(int value){
   case 0: //a,b,c,d,e,f should be OFF (common anode)
     PORTD = 0b10000001;
     break; //TODO : complete for other values.
+  case 1:
+    PORTD = 0b11110011;
+    break;
+  case 2:
+    PORTD = 0b01001001;
+    break;
+  case 3:
+    PORTD = 0b01100001;
+    break;
+  case 4:
+    PORTD = 0b00110011;
+    break;
+  case 5:
+    PORTD = 0b00100101;
+    break;
+  case 6:
+    PORTD = 0b00000101;
+    break;
+  case 7:
+    PORTD = 0b11110001;
+    break;
+  case 8:
+    PORTD = 0b00000001;
+    break;
+  case 9:
+    PORTD = 0b00100001;
+    break;
+  // find how the led works actually
   default:
     PORTD = 0b00000000; 
   }
 }
 
 void change_7seg_state (){
-  return;
+  PORTD ^= 0b00100000;
 }
   
 int main(void)
 {
   init();
+  int value = 0;
   while(1) //infinite loop
     {
-      display_7seg(0); // uncomment to test the 7-seg when DDRD is configured
-      change_led_state();
+      display_7seg(value);
+      // value = (value + 1) % 10;      
+      value = rand() % 10;
+
+       // uncomment to test the 7-seg when DDRD is configured
+      if (get_reset_value()) {
+        change_led_state();
+        value = 0;
+      }
+
       _delay_ms(1000);     // 1Hz period
     }
   
   return 0;
 }
+
